4659ef69f0f5621ddf0fcbdf07cf6c69eb1e15b3
ПК <@192.168.1.104>
2020-10-22 14:05:01 +0300

    t2

--- src/test/CustomerTest.java
+++ src/test/CustomerTest.java
@@ -6,6 +6,11 @@ public class CustomerTest {
 	
 	public static void main(String[] args) {
 		Customer customer = new Customer();
+		
+		customer.setID(10);
+		customer.setNew(false);
+		customer.setTotal(100.5);
+		
 		customer.displayCustomerInfo();
 	}
 }
